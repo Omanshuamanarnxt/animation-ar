efb474b883216c5e4486bcf0a056f6a5ba5ec329
Omanshuamanarnxt <omanshuaman@email.com>
2023-07-12 15:03:31 +0530

    play and pause animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 const App = () => {
   const modelViewerRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
     const modelViewer = document.querySelector("#change-speed-demo");
@@ -16,10 +17,23 @@ const App = () => {
     modelViewer.addEventListener("load", play);
     modelViewer.addEventListener("finished", play);
   }, []);
+
+  const handleToggleAnimation = () => {
+    const modelViewer = modelViewerRef.current;
+    if (modelViewer) {
+      if (isPlaying) {
+        modelViewer.pause();
+      } else {
+        modelViewer.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
   //test
   return (
     <div>
       <model-viewer
+        ref={modelViewerRef}
         id="change-speed-demo"
         camera-controls
         touch-action="pan-y"
@@ -28,8 +42,11 @@ const App = () => {
         ar-modes="webxr scene-viewer"
         scale="0.2 0.2 0.2"
         shadow-intensity="1"
-        src="https://jobpostingbucket.s3.ap-south-1.amazonaws.com/wallsandfloor/RobotExpressive.glb"
+        src="./src/assets/RobotExpressive.glb"
         alt="An animate 3D model of a robot"></model-viewer>
+      <button onClick={handleToggleAnimation}>
+        {isPlaying ? "Pause" : "Play"}
+      </button>
     </div>
   );
 };
